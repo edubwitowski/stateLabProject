8c5fe332714a4fab1d3b7b9668cbb2e0187fcd3a
Eric Witowski <ewitowski@students.mchenry.edu>
2018-10-22 18:18:32 -0500

    Initial Commit

---
+++ stateLabProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stateLabProject
+//
+//  Created by Macbook on 10/22/18.
+//  Copyright © 2018 Eric Witowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
